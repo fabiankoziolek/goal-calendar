11446b0dcc0111e78a7efafaa86c2abf124fd008
Alex Toryanik <alex1toryanik@gmail.com>
2019-01-14 20:36:30 +0100

    feature(utilities) add global exception handler

---
+++ GoalCalendar/GoalCalendar/Utilities/Exceptions/CustomErrorResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoalCalendar.Utilities.Exceptions
+{
+    public class CustomErrorResponse
+    {
+        public string Message { get; set; }
+        public string Description { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj is CustomErrorResponse errorResponse)
+            {
+                return Message == errorResponse.Message && Description == errorResponse.Description;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Message != null ? Message.GetHashCode() : 0) * 397) ^ (Description != null ? Description.GetHashCode() : 0);
+            }
+        }
+    }
+}
---
+++ GoalCalendar/GoalCalendar/Utilities/Exceptions/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace GoalCalendar.Utilities.Exceptions
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context /* other dependencies */)
+        {
+            try
+            {
+                await _next(context).ConfigureAwait(false);
+            }
+            catch (System.Exception ex)
+            {
+                await HandleExceptionAsync(context, ex).ConfigureAwait(false);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, System.Exception exception)
+        {
+            var response = context.Response;
+            var statusCode = (int)HttpStatusCode.InternalServerError;
+            var message = "Unexpected error";
+            var description = "Unexpected error";
+
+            if (exception is BaseHttpException customException)
+            {
+                message = customException.Message;
+                description = customException.Description;
+                statusCode = customException.Code;
+            }
+
+            response.ContentType = "application/json";
+            response.StatusCode = statusCode;
+            return response.WriteAsync(JsonConvert.SerializeObject(new CustomErrorResponse
+            {
+                Message = message,
+                Description = description,
+            }));
+        }
+    }
+}
