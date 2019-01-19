74b73f8d96fb5f3a11308a652e044cb7983a6887
Alex Toryanik <alex1toryanik@gmail.com>
2019-01-19 19:59:50 +0100

    fix(utilities) add ExceptionHandlingMiddleware to Startup.cs

--- GoalCalendar/GoalCalendar/Startup.cs
+++ GoalCalendar/GoalCalendar/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using GoalCalendar.Infrastructure.Database;
 ﻿using GoalCalendar.Utilities.Swagger;
 using GoalCalendar.Utilities.AutomaticDI;
+using GoalCalendar.Utilities.Exceptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -41,6 +42,7 @@ namespace GoalCalendar
                 app.UseHsts();
             }
 
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
