7f1922ea0e4ec6455c89072c27b91e763d4dc68d
Fabian Koziołek <fabiko25@gmail.com>
2019-01-19 13:33:20 +0100

    refactor(core) remove unused directives, rename db to context

--- GoalCalendar/GoalCalendar/Core/Note/NotesService.cs
+++ GoalCalendar/GoalCalendar/Core/Note/NotesService.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using GoalCalendar.Core.Note.TimeRange;
-using GoalCalendar.Core.Note.TimeRange.Strategies;
 using GoalCalendar.Core.Note.TimeRange.Strategies.Interface;
 using GoalCalendar.Core.Note.Web;
 using GoalCalendar.Infrastructure.Database;
@@ -14,31 +11,30 @@ namespace GoalCalendar.Core.Note
 {
     public class NotesService : INotesService
     {
-        private readonly GoalCalendarContext _db;
-        private IRangeStrategy _rangeStrategy;
+        private readonly GoalCalendarContext _context;
+        private ITimeRangeStrategy _timeRangeStrategy;
 
-        public NotesService(GoalCalendarContext db)
+        public NotesService(GoalCalendarContext context)
         {
-            _db = db;
+            _context = context;
         }
 
         public async Task<Note> Get(int id)
         {
             if (await Exists(id))
-                return await _db.Notes.FirstOrDefaultAsync(n => n.Id.Equals(id));
+                return await _context.Notes.FirstOrDefaultAsync(n => n.Id.Equals(id));
             throw new ObjectNotFoundException("Not found", $"Note with id: {id} not found");
         }
 
         public async Task<IList<Note>> Get(NoteRangeRequest request)
         {
-            SetRangeStrategy(request.Range);
-            return await _rangeStrategy.GetByRange(request.DateTime, request.UserId);
+            return await _timeRangeStrategy.GetByRange(request.DateTime, request.UserId);
         }
 
         public async Task Add(Note note)
         {
-            await _db.Notes.AddAsync(note);
-            await _db.SaveChangesAsync().ConfigureAwait(false);
+            await _context.Notes.AddAsync(note);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
         }
 
         public async Task Update(Note note, int id)
@@ -46,44 +42,27 @@ namespace GoalCalendar.Core.Note
             if (!await Exists(id)) throw new ObjectNotFoundException("Not found", $"Note with id: {id} not found");
             var noteToUpdate = await Get(id);
             noteToUpdate.Update(note);
-            _db.Notes.Update(note);
-            await _db.SaveChangesAsync().ConfigureAwait(false);
+            _context.Notes.Update(note);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
         }
 
         public async Task Delete(int id)
         {
             if (!await Exists(id)) throw new ObjectNotFoundException("Not found", $"Note with id: {id} not found");
-            var noteToRemove = _db.Notes.FirstOrDefault(n => n.Id.Equals(id));
-            _db.Notes.Remove(noteToRemove);
-            _db.SaveChanges();
+            var noteToRemove = _context.Notes.FirstOrDefault(n => n.Id.Equals(id));
+            _context.Notes.Remove(noteToRemove);
+            _context.SaveChanges();
         }
 
         private async Task<bool> Exists(int id)
         {
-            var exists = _db.Notes.AnyAsync(n => n.Id.Equals(id));
+            var exists = _context.Notes.AnyAsync(n => n.Id.Equals(id));
             return await exists;
         }
 
-
-        private void SetRangeStrategy(Range range)
+        private void SetRangeStrategy(ITimeRangeStrategy timeRangeStrategy)
         {
-            switch (range)
-            {
-                case Range.Day:
-                    _rangeStrategy = new DayStrategy(_db);
-                    break;
-                case Range.Week:
-                    _rangeStrategy = new WeekStrategy(_db);
-                    break;
-                case Range.Month:
-                    _rangeStrategy = new MonthStrategy(_db);
-                    break;
-                case Range.Year:
-                    _rangeStrategy = new YearStrategy(_db);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(range), range, null);
-            }
+            _timeRangeStrategy = timeRangeStrategy;
         }
     }
 }
