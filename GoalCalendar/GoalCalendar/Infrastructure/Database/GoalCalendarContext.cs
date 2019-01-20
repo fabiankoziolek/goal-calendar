f71da2f9b87e0a66791b2936c99185f90ebcf1a8
Alex Toryanik <alex1toryanik@gmail.com>
2019-01-20 10:41:37 +0100

    bugfix(infrastructure) remove rebundant INotesRepository

--- GoalCalendar/GoalCalendar/Infrastructure/Database/GoalCalendarContext.cs
+++ GoalCalendar/GoalCalendar/Infrastructure/Database/GoalCalendarContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GoalCalendar.Infrastructure.Database
 {
-    public class GoalCalendarContext : DbContext , INotesRepository
+    public class GoalCalendarContext : DbContext
     {
         public GoalCalendarContext(DbContextOptions options) : base(options)
         {
