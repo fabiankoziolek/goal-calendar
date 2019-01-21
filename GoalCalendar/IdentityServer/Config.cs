154dc0d2a1804e6a33f85938a2068f3b730ee5e0
Alex Toryanik <alex1toryanik@gmail.com>
2019-01-21 12:38:23 +0100

    feature(identityServer) add Config class with GetApiResource helper method

---
+++ GoalCalendar/IdentityServer/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("goalCalendarApi", "GoalCalendarApi")
+            };
+        }
+    }
+}
